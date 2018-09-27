e9f3f17d7613625a02fcffdcdb3cb320b2b5db72
LuisSanjuan <luisenrquesanjuan@gmail.com>
2018-09-27 18:13:39 -0500

    backend-restaurant using node, express &  mongo

---
+++ src/config/global.ts
@@ -0,0 +1,8 @@
+export const config = {
+    database:{
+        host:"mongodb://dario:dario@ds245755.mlab.com",
+        port:45755,
+        database:"restaurantes-db"
+    },
+    secret:'R35taurant35'
+}
---
+++ src/services/table-io-service.ts
@@ -0,0 +1,33 @@
+import * as socketIO from 'socket.io';
+import { Server } from 'http';
+
+
+export class TableIoService {
+
+    tables: SocketIO.Namespace;
+
+    init(server: Server) {
+        let io = socketIO(server);
+
+        this.tables = io.of("socket/tables");
+        this.tables.on("connection", (socket) => {
+            socket.on("subscribe", (id) => {
+                socket.join(id);
+            });
+            socket.on("unsubscribe", (id) => {
+                socket.leave(id);
+            });
+        });
+    }
+
+    changeAvailable(id: string, table: number,
+        available: boolean) {
+
+        this.tables.to(id)
+            .emit("available",
+            { mesa: table, disponible: available });
+    }
+
+}
+
+export const tableIO = new TableIoService();
